dbde1405973654fb0bb20082aedc02b268eedd40
Vinod <vinod@konflo.com>
2023-07-13 14:57:57 -0700

    Added weight init - still NaN losses

--- lemoncake/model.py
+++ lemoncake/model.py
@@ -12,6 +12,22 @@ from torchmetrics import (
     AveragePrecision,
 )
 
+# A function to initialize the weights of the model
+def init_multimodalbert(m, initrange, zero_bn=False):
+    """Initialize Multimodal BERT."""
+    if isinstance(m, (nn.Embedding, nn.EmbeddingBag)):
+        m.weight.data.uniform_(-initrange, initrange)
+    if isinstance(m, (nn.TransformerEncoderLayer, nn.Linear)):
+        for name, param in m.named_parameters():
+            if "bias" in name:
+                nn.init.constant_(param, 0.0)
+            elif "weight" in name:
+                nn.init.kaiming_normal_(param)
+    if isinstance(m, (nn.BatchNorm1d)):
+        nn.init.constant_(m.weight, 0.0 if zero_bn else 1.0)
+    for l in m.children():
+        init_multimodalbert(l, initrange, zero_bn)
+
 
 # Create a BERT model class in PyTorch with default values of 12 encoder layers, 12 attention heads, and hidden size 786
 class BERT(nn.Module):
@@ -120,6 +136,9 @@ class MultimodalBERT(pl.LightningModule):
         )
         self.predictor = MultiLabelPredictor(hidden=hidden)
 
+        # init weights from Karpathy's nanoGPT (see below)
+        self.apply(self._init_weights)
+
         ## metrics
         # metrics = MetricCollection(
         #     [
@@ -134,11 +153,24 @@ class MultimodalBERT(pl.LightningModule):
         # self.valid_metrics = metrics.clone(prefix="valid/")
         # self.test_metrics = metrics.clone(prefix="test/")
 
+
+    ## init weights from Karpathy's nanoGPT
+    ## https://github.com/karpathy/nanoGPT/blob/a82b33b525ca9855d705656387698e13eb8e8d4b/model.py#L147
+    def _init_weights(self, module):
+            if isinstance(module, nn.Linear):
+                # torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
+                nn.init.xavier_normal_(module.weight)
+                if module.bias is not None:
+                    torch.nn.init.zeros_(module.bias)
+            # elif isinstance(module, nn.Embedding):
+            #     torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
+
+
     def forward(self, x):  # x: (B, 4041)
         x = self.preprocessor(x)  # x: (B, 256, 768)
         x = self.bert(x)  # x: (B, seq_len, hidden)
-        x = self.predictor(x)  # x: (B, 14)
-        return x  # x: (B, 14)
+        x = self.predictor(x)  # x: (B, 13)
+        return x  # x: (B, 13)
 
     def training_step(self, batch, batch_idx):
         x, y = batch["x"], batch["y"]
